33c4edbda65eea557eb8266c8690badc3a269e5f
siva-manohar <132001142+siva-manohar@users.noreply.github.com>
2023-06-12 10:49:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ docker ps -a
 docker rm -f dotnet-app | ls -la
 docker ps -a
 docker run -itd -p 3000:3000 --name dotnet-app 933542948767.dkr.ecr.ap-southeast-1.amazonaws.com/docker1:$1
+docker ps -a
