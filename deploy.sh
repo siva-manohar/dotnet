a348cc72338a5a610706fd3ed301feb9496a18eb
siva-ixfiworld <sivachaithanya.k@ixfi.com>
2023-07-14 07:10:15 +0530

    changed the account ecr repository

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com
 
-docker pull 933542948767.dkr.ecr.ap-southeast-1.amazonaws.com/docker1:$1
+docker pull 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com/docker-automation:$1
 echo "docker pull succeeded"
 
 docker ps -a
@@ -12,7 +12,7 @@ echo "delete the container "
 docker ps -a
 echo "empty docker"
 
-docker run -itd -p 3000:3000 --name dotnet-app 092101872227.dkr.ecr.ap-southeast-1.amazonaws.comdocker:$1
+docker run -itd -p 3000:3000 --name dotnet-app 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com/docker-automation:$1
 
 docker ps -a
 echo "docker container deployed successfully"
