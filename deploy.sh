7bf1cc1a70aa0ead79eb6f9d4e308f5654803cea
Siva-ixfiworld <sivachaithanya.k@ixfi.com>
2024-09-03 08:01:54 +0530

    added the remove container

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 # Docker pull image
 docker pull 590183962065.dkr.ecr.us-east-1.amazonaws.com/docker_project:$1
 
+#delete the existing container
+docker rm -f dotnet-app
+
 # dokcer run the image
 docker run -itd -p 8000:8000 --name dotnet-app 590183962065.dkr.ecr.us-east-1.amazonaws.com/docker_project:$1
